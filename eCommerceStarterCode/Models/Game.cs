2a3e19c880d277556259cee21f39b725215a0ea7
unknown <dylanbram354@gmail.com>
2021-07-14 15:38:17 -0400

    Group programming - Added Game and Platform classes

---
+++ eCommerceStarterCode/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        
+        [ForeignKey("Platform")]
+        public int PlatformId { get; set; }
+        public PlatformID Platform { get; set; }
+    }
+}
---
+++ eCommerceStarterCode/Models/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Platform
+    {
+        public int PlatformId { get; set; }
+        public string Name { get; set; }
+    }
+}
