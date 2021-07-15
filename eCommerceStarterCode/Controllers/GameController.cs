a1022f2d3d061b663c510ff3083c5f16c5f21926
unknown <dylanbram354@gmail.com>
2021-07-15 12:11:50 -0400

    (Group programming) Added endpoint to post new game

---
+++ eCommerceStarterCode/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/games")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public GameController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost, Authorize]
+
+        public IActionResult PostNewGame([FromBody]Game value)
+        {
+            _context.Games.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+
+        }
+    }
+}
